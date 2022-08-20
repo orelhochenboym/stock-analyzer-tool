5b159d44c0c52671baae91831382550003a74071
Orel Hochenboym <orelhochenboym@gmail.com>
2022-08-20 11:24:43 +0300

    finished header

--- src/components/HeaderHistorical/HeaderHistorical.tsx
+++ src/components/HeaderHistorical/HeaderHistorical.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './HistoricalHeader.scss';
+import './HeaderHistorical.scss';
 
 export default function HistoricalHeader() {
 	return (
-		<div id='historical-header'>
+		<div id='historical-container'>
 			<div id='historical'>Historical</div>
 			<div id='one-year'>1 Year</div>
 			<div id='five-year'>5 Year</div>
--- src/components/HeaderMyAssumptions/HeaderMyAssumptions.tsx
+++ src/components/HeaderMyAssumptions/HeaderMyAssumptions.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './MyAssumptionsHeader.scss';
+import './HeaderMyAssumptions.scss';
 
 export default function MyAssumptionsHeader() {
 	return (
-		<div id='my-assumptions-header'>
+		<div id='my-assumptions-container'>
 			<div id='my-assumptions'>My Assumptions</div>
 			<div id='low'>Low</div>
 			<div id='mid'>Mid</div>
